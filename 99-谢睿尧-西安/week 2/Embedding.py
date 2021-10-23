f6d662d5d9e2d75640aac768d4ea717efbd78176
knowyourgame-ry <ruiyaoxie@outlook.com>
2021-10-23 15:26:09 +0800

    第二次作业 week2 part2

---
+++ 99-谢睿尧-西安/week 2/Embedding.py
@@ -0,0 +1,18 @@
+#coding:utf8
+import torch
+import torch.nn as nn
+
+'''
+embedding层的处理
+'''
+
+num_embeddings = 6  # 通常对于nlp任务，此参数为字符集字符总数
+embedding_dim = 4   # 每个字符向量化后的向量维度
+embedding_layer = nn.Embedding(num_embeddings, embedding_dim)
+print(embedding_layer.weight, "随机初始化权重")
+
+#构造输入
+x = torch.LongTensor([[1,2,3,4], [2,2,0,4], [3,2,1,4]])
+embedding_out = embedding_layer(x)
+print(embedding_out)
+
